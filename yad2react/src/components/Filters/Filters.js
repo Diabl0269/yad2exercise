fefd3408193f3a58e7abcddb8d722cd49b347dea
Tal <talefronny@gmail.com>
2020-02-26 16:44:48 +0200

    started working on advanced filters block

--- yad2react/src/components/Filters/Filters.js
+++ yad2react/src/components/Filters/Filters.js
@@ -3,51 +3,59 @@ import ListingsTypeContext from "../../context/ListingsTypeContext";
 import RoomsFilter from "./RoomsFilter";
 import CitiesFilter from "./CitiesFilter";
 import PropertyTypeFilter from "./PropertyTypeFilter";
-import PriceFilter from './PriceFilter';
+import PriceFilter from "./PriceFilter";
 
 const Filters = () => {
   const { currentListingsType, dispatch } = useContext(ListingsTypeContext);
   const [citySearchValue, setCitySearchValue] = useState();
   const [selectedAssetTypes, setSelectedAssetType] = useState([]);
   const [roomsNumber, setRooms] = useState({ min: undefined, max: undefined });
-  const [price, setPrice] = useState({min: undefined, max: undefined});
+  const [price, setPrice] = useState({ min: "", max: "" });
 
   return (
-    <div className="filters-container">
-      <b>
-        איזה נכס ל
-        <span onClick={() => alert("כאן יפתח חלון בחירת קטגוריה סחר")}>
-          {currentListingsType}
-        </span>{" "}
-        תרצו לחפש?
-      </b>
+    <div>
+      <div className="filters__basic-container">
+        <b>
+          איזה נכס ל
+          <span onClick={() => alert("כאן יפתח חלון בחירת קטגוריה סחר")}>
+            {currentListingsType}
+          </span>{" "}
+          תרצו לחפש?
+        </b>
 
-      <div className="filters--fields-container smaller-text">
-        <CitiesFilter />
+        <div className="filters--fields-container smaller-text">
+          <CitiesFilter />
 
-        {currentListingsType === "שותפים" ? (
-          <div className="filters--field-container">שותפים</div>
-        ) : (
-          <PropertyTypeFilter
-            selectedAssetTypes={selectedAssetTypes}
-            setSelectedAssetType={setSelectedAssetType}
-          />
-        )}
+          {currentListingsType === "שותפים" ? (
+            <div className="filters--field-container">שותפים</div>
+          ) : (
+            <PropertyTypeFilter
+              selectedAssetTypes={selectedAssetTypes}
+              setSelectedAssetType={setSelectedAssetType}
+            />
+          )}
 
-        {currentListingsType === "מסחרי" ? (
-          <div className="filters--field-container">בחרו עסקה</div>
-        ) : (
-          <RoomsFilter roomsNumber={roomsNumber} setRooms={setRooms} />
-        )}
-        <PriceFilter price={price} setPrice={setPrice} />
+          {currentListingsType === "מסחרי" ? (
+            <div className="filters--field-container">בחרו עסקה</div>
+          ) : (
+            <RoomsFilter roomsNumber={roomsNumber} setRooms={setRooms} />
+          )}
+          <PriceFilter price={price} setPrice={setPrice} />
 
-        <button className="filters--field-container filters--advanced-search-button">
-          חיפוש מתקדם
-        </button>
-        <button className="filters--field-container main-nav-bar-button">
-          חיפוש
-        </button>
+          <button className="filters--field-container filters--advanced-search-button">
+            חיפוש מתקדם
+          </button>
+          <button className="filters--field-container main-nav-bar-button">
+            חיפוש
+          </button>
+        </div>
       </div>
+      <div className='filters__advanced-dropdown-container'>
+            <div className='filters__advanced--asset-attributes'>א</div>
+            <div className='filters__advanced--asset-details'>ב</div>
+            <div className='filters__advanced--free-text'>ג</div>
+            <div className='filters__advanced--search'>ד</div>
+       </div>      
     </div>
   );
 };
--- yad2react/src/components/Filters/PriceFilter.js
+++ yad2react/src/components/Filters/PriceFilter.js
@@ -1,14 +1,32 @@
-import React from 'react';
+import React from "react";
 
-const PriceFilter = ({price, setPrice}) => {
-    return (
-        <div className="filters--field-container">מחיר בש"ח
-        <div className='filters__price-container'>
-            <div className='filters__price-inner-container'></div>
-            <div className='filters__price-inner-container'></div>
-        </div>
-        </div>
-    )
-}
+const PriceFilter = ({ price, setPrice }) => {
+  const handleSetPrice = ({ id, value }) => {
+    if ((value === "") | !isNaN(value[value.length - 1]))
+      setPrice({ ...price, [id]: value });
+  };
+
+  return (
+    <div className="filters--field-container">
+      מחיר בש"ח
+      <div className="filters__price-container">
+        <input
+          type="text"
+          id="min"
+          value={price.min}
+          onChange={e => handleSetPrice(e.target)}
+          className="filters__price-inner-container"
+        />
+        <input
+          type="number"
+          value={price.max}
+          onChange={e => handleSetPrice(e.target)}
+          id="max"
+          className="filters__price-inner-container"
+        />
+      </div>
+    </div>
+  );
+};
 
 export default PriceFilter;
