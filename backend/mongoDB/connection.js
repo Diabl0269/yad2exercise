52680fc7bc767aa149caa543ef948cd787d894cc
Tal Efronny <talefronny@gmail.com>
2020-03-30 19:19:08 +0300

    fixed start script

--- backend/mongoDB/connection.js
+++ backend/mongoDB/connection.js
@@ -1,19 +1,21 @@
-require("dotenv").config({ path: __dirname + "/../.env" });
-const mongoose = require("mongoose");
-const { success, failure } = require('../utils/messageColor');
+const mongoose = require('mongoose')
+const { success, failure } = require('../utils/messageColor')
 
-const successMsg = success('mongo connection exists');
-const errorMsg = failure('connection error:');
+if (process.env.NODE_ENV !== 'production')
+  require('dotenv').config({ path: __dirname + '/../.env' })
 
-const db = mongoose.connection;
-db.on("error", console.error.bind(console, errorMsg));
-db.once("open", function() {
-  console.log(successMsg);
-});
+const successMsg = success('mongo connection exists')
+const errorMsg = failure('connection error:')
+
+const db = mongoose.connection
+db.on('error', console.error.bind(console, errorMsg))
+db.once('open', function() {
+  console.log(successMsg)
+})
 
 mongoose.connect(process.env.DB_URL, {
   useNewUrlParser: true,
   useUnifiedTopology: true
-});
+})
 
-mongoose.set('useCreateIndex', true);
+mongoose.set('useCreateIndex', true)
